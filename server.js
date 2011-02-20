75c1b187f9b13668f3e6a624c369753751111a8b
Simeon Bateman <simeon@simb.net>
2011-02-20 09:34:22 -0800

    adding server and client files

---
+++ server.js
@@ -0,0 +1,33 @@
+require.paths.unshift(__dirname + '/lib');
+
+var http = require('express'),  
+    io = require('socket.io'),
+	sys = require('sys');
+
+server = http.createServer(
+	http.staticProvider(__dirname + '/public')
+);
+server.listen(8124);
+  
+// socket.io 
+var socket = io.listen(server); 
+
+	
+  	// Add a connect listener
+	socket.on('connection', function(client){ 
+		
+		var interval = setInterval(function() {
+		  client.send('This is a message from the server!  ' + new Date().getTime());
+		},5000);
+	  // Success!  Now listen to messages to be received
+	  client.on('message',function(event){ 
+	    console.log('Received message from client!',event);
+		client.send('Echo: ' + event );
+	  });
+	  client.on('disconnect',function(){
+	    //clearInterval(interval);
+	    console.log('Server has disconnected');
+	  });
+	
+	
+});
